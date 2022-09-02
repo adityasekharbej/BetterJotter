28bc5b6b19e4177b27cb6d9af742cbff38c74303
Santanu Mohapatra <99542723+Santanu55555@users.noreply.github.com>
2022-09-02 18:07:06 +0530

    ok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,14 @@ import Home from "./components/Homepage/Home";
 import { MainRoutes } from "./Routes/MainRoutes";
 import Task from './pages/Task';
 import About from './pages/About';
+import C from "./components/about/C";
 
 function App() {
   return (
     <div>
-      <MainRoutes />
-      <Task/>
-      <About/>
+      {/* <MainRoutes /> */}
+      {/* <Task/> */}
+      <C/>
       {/* <Home/> */}
     </div>
   );
