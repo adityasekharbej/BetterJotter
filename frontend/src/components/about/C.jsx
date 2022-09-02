c991b29fb82fe1f5da7e77cef590f56f52c5ce1b
Santanu Mohapatra <99542723+Santanu55555@users.noreply.github.com>
2022-09-03 02:15:46 +0530

    ok

--- frontend/src/components/about/C.jsx
+++ frontend/src/components/about/C.jsx
@@ -1,20 +1,84 @@
-import { Box, Flex, Heading, HStack, VStack } from '@chakra-ui/react'
-import React from 'react'
-import "./C.css"
+import { Box, Flex, Heading, HStack, VStack } from "@chakra-ui/react";
+import React from "react";
+import "./C.css";
 const C = () => {
   return (
-    <Box h={[500, 600, 700]} w={[1100, 1200, 1300]} m="auto"p="auto">
-        <HStack justify ="center" h="50%">
-            <Box w="40%"bgSize="100%" h="90%"className='C'  backgroundImage="url('https://i.postimg.cc/wjLrjCmC/99960489.jpg')"   bgPosition="center"
-  bgRepeat="no-repeat"><Heading mt="25%" >Susmita</Heading></Box>
-            <Box w="40%" bgSize="100%"h="90%"  backgroundImage="url('https://i.postimg.cc/L8YQhnJQ/Whats-App-Image-2022-09-02-at-5-54-07-PM.jpg')"   bgPosition="center"
-  bgRepeat="no-repeat"><Heading>Aditya</Heading></Box>
-           
-        </HStack>
-        <Flex justify="center"h="50%" > <Box bgSize="100%"w="35%" className='C' h="80%"  backgroundImage="url('https://i.postimg.cc/BQ46Q0kF/resumepic.png')"   bgPosition="center"
-  bgRepeat="no-repeat"><Heading>Santanu</Heading></Box></Flex>
-	</Box>
-  )
-}
+    <Box h={[500, 600, 700]} w={[1100, 1200, 1300]} m="auto" p="auto">
+      <HStack justify="center" h="50%">
+        <Box
+          w="40%"
+          bgSize="100%"
+          mr="1%"
+          h="95%"
+          className="C"
+          backgroundImage="url('https://i.postimg.cc/wjLrjCmC/99960489.jpg')"
+          role="group"
+          bgPosition="center"
+          bgRepeat="no-repeat"
+        >
+          <Heading
+            w="100%"
+            h="100%"
+            pt="35%"
+            align="right"
+            fontSize={"50px"}
+            mr="2%"
+            _groupHover={{ backdropFilter: "none" }}
+            className="animate-charcter"
+            _hover={{ fontSize:"40px", cursor: "pointer" }}
 
-export default C
+          >
+            Susmita
+          </Heading>
+        </Box>
+        <Box
+          w="40%"
+          bgSize="100%"
+          h="95%"
+          role="group"
+          backgroundImage="url('https://i.postimg.cc/L8YQhnJQ/Whats-App-Image-2022-09-02-at-5-54-07-PM.jpg')"
+          bgPosition="center"
+          bgRepeat="no-repeat"
+        >
+          <Heading    w="100%"
+            h="100%"
+            fontSize={"50px"}
+            pt="35%"  ml="2%"
+            _groupHover={{ backdropFilter: "none" }}
+            _hover={{ fontSize:"40px", cursor: "pointer" }}
+            className="animate-charcter">
+            Aditya
+          </Heading>
+        </Box>
+      </HStack>
+      <Flex justify="center" h="50%">
+        {" "}
+        <Box
+          bgSize="100%"
+          w="40%"
+          className="C"
+          h="100%"
+          role="group"
+          backgroundImage="url('https://i.postimg.cc/BQ46Q0kF/resumepic.png')"
+          bgPosition="center"
+          bgRepeat="no-repeat"
+        >
+          <Heading    w="100%"
+            h="100%"
+            pt="45%"
+          
+            align="center"
+            fontSize={"50px"}
+            _groupHover={{ backdropFilter: "none" }}
+            _hover={{ fontSize:"40px", cursor: "pointer", paddingTop:"55%",
+            }}
+             className="animate-charcter">
+            Santanu
+          </Heading>
+        </Box>
+      </Flex>
+    </Box>
+  );
+};
+
+export default C;
--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -14,9 +14,15 @@ import {
   ModalCloseButton,
   useDisclosure,
   ModalBody,
-  Flex
+  Flex,
+  Image,
+  Center,
+  Heading,
+  VStack,
+  Text,
 } from "@chakra-ui/react";
-
+import { Link as Link } from "react-router-dom";
+import "../components/about/C.css";
 const names = [
   { id: "1", name: "Aditya" },
   { id: "2", name: "Santanu" },
@@ -28,80 +34,191 @@ const About = () => {
   return (
     <>
       <Box>
-        <Accordion allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton _expanded={{ bg: "tomato", color: "white" }}>
-                <Box flex="1" textAlign="left">
-                  Aditya
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </AccordionPanel>
-          </AccordionItem>
+      
+          <Center>
+            <Heading className="btn-shine">Made with LOVE ❤️</Heading>
+         
 
-          <AccordionItem>
-            <h2>
-              <AccordionButton _expanded={{ bg: "tomato", color: "white" }}>
-                <Box flex="1" textAlign="left">
-                  Santanu
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </AccordionPanel>
-          </AccordionItem>
 
-          <AccordionItem>
-            <h2>
-              <AccordionButton _expanded={{ bg: "tomato", color: "white" }}>
-                <Box flex="1" textAlign="left">
-                  Susmita
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
+          </Center>
+          <Accordion allowMultiple>
+            <AccordionItem>
+              <h2>
+                <AccordionButton _expanded={{ bg: "black", color: "white" }}>
+                  <Image
+                    borderRadius="100%"
+                    h="100px"
+                    w="100px"
+                    src="https://i.postimg.cc/L8YQhnJQ/Whats-App-Image-2022-09-02-at-5-54-07-PM.jpg"
+                  />
+                  <Box flex="1" className="animate-charcter">
+                    Aditya
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel
+                pb={4}
+                bg="lightgray"
+                align="center"
+                m="auto"
+                p="auto"
+              >
+                <Center>
+                  {" "}
+                  <ul>
+                    <li>
+                      <a
+                        target="open"
+                        href="https://www.facebook.com/aditya.sekhar.bej/"
+                      >
+                        <i class="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        target="open"
+                        href="https://www.linkedin.com/in/aditya-sekhar-bej/"
+                      >
+                        <i class="fab fa-linkedin-in"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        target="open"
+                        href="https://github.com/adityasekharbej"
+                      >
+                        <i class="fab fa-github"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        target="open"
+                        href="https://www.instagram.com/i.maverick.exe/"
+                      >
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </Center>
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton _expanded={{ bg: "black", color: "white" }}>
+                  <Image
+                    borderRadius="100%"
+                    fit="cover"
+                    h="100px"
+                    w="100px"
+                    src="https://i.postimg.cc/BQ46Q0kF/resumepic.png"
+                  />
+                  <Box flex="1" className="animate-charcter">
+                    Santanu
+                  </Box>{" "}
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel
+                pb={4}
+                bg="lightgray"
+                align="center"
+                m="auto"
+                p="auto"
+              >
+                <Center>
+                  {" "}
+                  <ul>
+                    <li>
+                      <a
+                        target="open"
+                        href="https://www.facebook.com/7008467376.shaaN/"
+                      >
+                        <i class="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+
+                    <li>
+                      <a
+                        target="open"
+                        href="https://www.linkedin.com/in/santanu-mohapatra-880204200/"
+                      >
+                        <i class="fab fa-linkedin-in"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a target="open" href="https://github.com/santanu55555">
+                        <i class="fab fa-github"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        target="open"
+                        href="https://www.instagram.com/shantanu_469/"
+                      >
+                        <i class="fab fa-instagram"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </Center>
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton _expanded={{ bg: "black", color: "white" }}>
+                  <Image
+                    borderRadius="100%"
+                    h="100px"
+                    w="100px"
+                    src="https://i.postimg.cc/wjLrjCmC/99960489.jpg"
+                  />
+                  <Box flex="1" className="animate-charcter">
+                    Susmita
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel
+                pb={4}
+                bg="lightgray"
+                align="center"
+                m="auto"
+                p="auto"
+              >
+                <Center>
+                  {" "}
+                  <ul>
+                    <li>
+                      <a
+                        href="https://www.facebook.com/susmita.mandal.50746"
+                        target="open"
+                      >
+                        <i class="fab fa-facebook-f"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        target="open"
+                        href="https://www.linkedin.com/in/susmitamandal549/"
+                      >
+                        <i class="fab fa-linkedin-in"></i>
+                      </a>
+                    </li>
+                    <li>
+                      <a target="open" href="https://github.com/Susmita549">
+                        <i class="fab fa-github"></i>
+                      </a>
+                    </li>
+                  </ul>
+                </Center>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+      
       </Box>
-      <Flex justify="center" gap={30}>
-        {names.map((el) => (
-          <Box key={el.id} >
-            <Button colorScheme='teal' onClick={onOpen}>{el.name}</Button>
-
-            <Modal isOpen={isOpen} onClose={onClose}>
-              <ModalOverlay />
-              <ModalContent>
-                <ModalHeader>{el.name}</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat.
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-          </Box>
-        ))}
-      </Flex>
     </>
   );
 };
